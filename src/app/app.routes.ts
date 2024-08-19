b8a20ff9c879dfe8bee9af809e94a5be98ecfb8a
Ridwanul Hafiz <ridwanul.hafiz@gmail.com>
2024-08-19 11:17:18 +0600

    auth page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./modules/game-portal/auth/login.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    component: LoginComponent
+  }
+];
