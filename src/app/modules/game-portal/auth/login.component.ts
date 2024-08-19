27bdf322947d525f282df21e729f10b978b55b9a
Ridwanul Hafiz <ridwanul.hafiz@gmail.com>
2024-08-19 12:47:34 +0600

    [01-stc-splash-screen-design]

--- src/app/modules/game-portal/auth/login.component.ts
+++ src/app/modules/game-portal/auth/login.component.ts
@@ -1,9 +1,13 @@
 
 import {Component} from "@angular/core";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  imports: [
+    NgOptimizedImage
+  ],
   standalone: true
 })
 export class LoginComponent {
